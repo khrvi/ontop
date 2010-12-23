663fd9b26f56b8c0d44db5680b9dbca5a81d58d5
Viacheslav Khrenov <khrviach@gmail.com>
2010-12-23 15:59:48 +0200

    added  validation of the capcha

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 # encoding: UTF-8
 class UsersController < ApplicationController
   before_filter :create_brain_buster, :only => :new
+  before_filter :validate_brain_buster, :only => :create
   def new 
   end
      
